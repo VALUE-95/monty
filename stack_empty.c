e6d42d0228af9740de070032f99629a7d9dc6621
Eze Israel John <ezeisrael499@gmail.com>
2023-12-14 11:24:01 +0100

    Added a function that checks the emptiness of a stack

---
+++ stack_empty.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * stack_empty - check if the stack is empty
+ * @stack: stack
+ *
+ * Return: 1 if empty, 0 if not
+ **/
+int stack_empty(stack_t *stack)
+{
+	return (stack == NULL);
+}
