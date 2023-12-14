6ffaf2e860687f98d260a56578b4f08f263f251d
Eze Israel John <ezeisrael499@gmail.com>
2023-12-14 14:14:49 +0100

    Function that implements the pchar program

---
+++ pchar.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pchar - print the top most value as a char if not out of range
+ * @stack: stack
+ * @line_num: line_num
+ */
+void pchar(stack_t **stack, unsigned int line_num)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
+}
